3343093640fd290f1bab072d591cca42dc2cada0
Matt Sahn <sahn44@yahoo.com>
2016-04-11 23:16:03 -0400

    update

--- python/dash/dash-action.py
+++ python/dash/dash-action.py
@@ -1,9 +1,22 @@
 from scapy.all import *
+from requests import post
+
+# Home Assistant API URL for starting script
+url = 'http://localhost:8123/api/services/script/turn_on'
+headers = {'x-ha-access': '',
+           'content-type': 'application/json'}
+json_data = {"entity_id": "script.arrive_home"}
+
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
       if pkt[ARP].hwsrc == 'f0:27:2d:1b:26:58': # Dash button
         print "Dash button pressed!"
+        ## call HA API
+        response = post(url, headers=headers, json=json_data)
+        print "API called"
+        print(response.text)
+        print "got API response"
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 print sniff(prn=arp_display, filter="arp", store=0)
